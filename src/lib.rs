420c2542757ff844a4c5b232ac49429c2aceb53d
Sandro-Alessio Gierens <sandro@gierens.de>
2023-08-28 15:37:25 +0200

    refactor(src): remove unnecessary unit test
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib.rs
+++ src/lib.rs
@@ -53,14 +53,3 @@ impl Api {
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
