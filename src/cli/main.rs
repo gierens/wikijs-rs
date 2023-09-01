7ef9e39a6a9c734a8cb542873f821957dbd40135
Sandro-Alessio Gierens <sandro@gierens.de>
2023-09-01 10:56:39 +0200

    feature(cli): add basic clap parser with url and api key arguments
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use clap::Parser;
 
+#[derive(Parser)]
+#[command(name = "wikijs-cli")]
+#[command(author = "Sandro-Alessio Gierens <sandro@gierens.de>")]
+#[command(version = "0.1.0")]
+#[command(about = "Command line client for Wiki.js")]
+struct Cli {
+    #[clap(short, long, help = "Wiki.js base URL", env = "WIKI_JS_BASE_URL")]
+    url: String,
+
+    #[clap(short, long, help = "Wiki.js API key", env = "WIKI_JS_API_KEY")]
+    key: String,
+}
+
+fn main() {
+    let _cli = Cli::parse();
 }
