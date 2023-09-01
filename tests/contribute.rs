015a46897219a88e1fd90e77b2fa6d9822e31757
Sandro-Alessio Gierens <sandro@gierens.de>
2023-09-01 21:19:10 +0200

    fix(tests): rename contributors test to contributor_list
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- tests/contribute.rs
+++ tests/contribute.rs
@@ -2,7 +2,7 @@ mod common;
 use common::API;
 
 #[test]
-fn contributors() {
+fn contributor_list() {
     // TODO this needs a more elaborate check
     let result = API.contributor_list();
     assert!(result.is_ok());
