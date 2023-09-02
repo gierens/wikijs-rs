6324c669d43e45fbf4ea8d544a0d9b62faa3879a
Sandro-Alessio Gierens <sandro@gierens.de>
2023-09-02 15:57:00 +0200

    feature(tests): add test page_create_get_delete
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- tests/page.rs
+++ tests/page.rs
@@ -1,6 +1,7 @@
 mod common;
 use common::API;
 
+use serial_test::serial;
 use wikijs::page::PageError;
 
 #[test]
@@ -50,4 +51,32 @@ fn page_render_nonexistent() {
     let result = API.page_render(1000000);
     assert!(result.is_err());
     assert_eq!(result.unwrap_err() == PageError::PageNotFound, true);
+}
+
+#[test]
+#[serial]
+fn page_create_get_delete() {
+    let result = API.page_create(
+        "...".to_string(),
+        "".to_string(),
+        "markdown".to_string(),
+        true,
+        false,
+        "en".to_string(),
+        "test".to_string(),
+        None,
+        None,
+        None,
+        None,
+        Vec::new(),
+        "test".to_string(),
+    );
+    assert!(result.is_ok());
+    let result2 = API.page_get_by_path("test".to_string(), "en".to_string());
+    assert!(result2.is_ok());
+    let id = result2.unwrap().id;
+    let result3 = API.page_delete(id);
+    assert!(result3.is_ok());
+    let result4 = API.page_get(id);
+    assert!(result4.is_err());
 }
