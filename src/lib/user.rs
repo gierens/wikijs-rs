f0c0a03c9336cf2fdd5ee65194c9a5d24af7fe37
Sandro-Alessio Gierens <sandro@gierens.de>
2023-09-06 19:46:51 +0200

    feature(lib): add UserError to user module
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/lib/user.rs
+++ src/lib/user.rs
@@ -1,7 +1,113 @@
 use serde::Deserialize;
+use thiserror::Error;
 
-use crate::common::{Boolean, Date, Int, ResponseStatus};
+use crate::common::{
+    Boolean, Date, Int, ResponseStatus, UnknownError, KnownErrorCodes
+};
 use crate::group::Group;
+
+#[derive(Debug, Error, PartialEq)]
+pub enum UserError {
+    #[error("An unexpected error occurred during login.")]
+    AuthGenericError,
+    #[error("Invalid email / username or password.")]
+    AuthLoginFailed,	
+    #[error("Invalid authentication provider.")]
+    AuthProviderInvalid,
+    #[error("An account already exists using this email address.")]
+    AuthAccountAlreadyExists,
+    #[error("Incorrect TFA Security Code.")]
+    AuthTFAFailed,
+    #[error("Invalid TFA Security Code or Login Token.")]
+    AuthTFAInvalid,
+    #[error("Invalid Brute Force Instance.")]
+    BruteInstanceIsInvalid,	
+    #[error("Too many attempts! Try again later.")]
+    BruteTooManyAttempts,
+    #[error("An unexpected error occurred during user creation.")]
+    UserCreationFailed,	
+    #[error("Registration is disabled. Contact your system administrator.")]
+    AuthRegistrationDisabled,
+    #[error("You are not authorized to register. Your domain is not whitelisted.")]
+    AuthRegistrationDomainUnauthorized,
+    #[error("Input data is invalid.")]
+    InputInvalid,
+    #[error("Your account has been disabled.")]
+    AuthAccountBanned,	
+    #[error("You must verify your account before your can login.")]
+    AuthAccountNotVerified,
+    #[error("Invalid validation token.")]
+    AuthValidationTokenInvalid,
+    #[error("This user does not exist.")]
+    UserNotFound,
+    #[error("Cannot delete user because of content relational constraints.")]
+    UserDeleteForeignConstraint,
+    #[error("Cannot delete a protected system account.")]
+    UserDeleteProtected,
+    #[error("You must be authenticated to access this resource.")]
+    AuthRequired,
+    #[error("Password is incorrect.")]
+    AuthPasswordInvalid,
+    #[error("Unknown response error code: {code}: {message}")]
+    UnknownErrorCode { code: i64, message: String },
+    #[error("Unknown response error: {message}")]
+    UnknownErrorMessage { message: String },
+    #[error("Unknown response error.")]
+    UnknownError,
+}
+
+impl From<i64> for UserError {
+    fn from(code: i64) -> Self {
+        match code {
+            1001 => UserError::AuthGenericError,
+            1002 => UserError::AuthLoginFailed,
+            1003 => UserError::AuthProviderInvalid,
+            1004 => UserError::AuthAccountAlreadyExists,
+            1005 => UserError::AuthTFAFailed,
+            1006 => UserError::AuthTFAInvalid,
+            1007 => UserError::BruteInstanceIsInvalid,
+            1008 => UserError::BruteTooManyAttempts,
+            1009 => UserError::UserCreationFailed,
+            1010 => UserError::AuthRegistrationDisabled,
+            1011 => UserError::AuthRegistrationDomainUnauthorized,
+            1012 => UserError::InputInvalid,
+            1013 => UserError::AuthAccountBanned,
+            1014 => UserError::AuthAccountNotVerified,
+            1015 => UserError::AuthValidationTokenInvalid,
+            1016 => UserError::UserNotFound,
+            1017 => UserError::UserDeleteForeignConstraint,
+            1018 => UserError::UserDeleteProtected,
+            1019 => UserError::AuthRequired,
+            1020 => UserError::AuthPasswordInvalid,
+            _ => UserError::UnknownErrorCode {
+                code,
+                message: "Unknown error".to_string(),
+            },
+        }
+    }
+}
+
+impl UnknownError for UserError {
+    fn unknown_error_code(code: i64, message: String) -> Self {
+        UserError::UnknownErrorCode { code, message }
+    }
+    fn unknown_error_message(message: String) -> Self {
+        UserError::UnknownErrorMessage { message }
+    }
+    fn unknown_error() -> Self {
+        UserError::UnknownError
+    }
+}
+
+impl KnownErrorCodes for UserError {
+    fn known_error_codes() -> Vec<i64> {
+        vec![8001, 8002, 8003, 8004, 8005, 8006]
+    }
+
+    fn is_known_error_code(code: i64) -> bool {
+        (6001..=6013).contains(&code)
+    }
+}
 
 #[derive(Deserialize, Debug)]
 pub struct UserResponse {
