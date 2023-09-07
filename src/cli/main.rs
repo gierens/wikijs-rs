c3a7309e9455b6cadc0a104110a13edd4890c7a5
Sandro-Alessio Gierens <sandro@gierens.de>
2023-09-07 15:27:33 +0200

    feature(cli): add locale list command
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -934,6 +934,42 @@ fn main() {
                 }
             }
         },
+        Command::Locale { command } => match command {
+            LocaleCommand::List {} => match api.locale_list() {
+                Ok(locales) => {
+                    let mut builder = Builder::new();
+                    builder.push_record([
+                        "availability",
+                        "code",
+                        "created_at",
+                        "install_date",
+                        "is_installed",
+                        "is_rtl",
+                        "name",
+                        "native_name",
+                        "updated_at"
+                    ]);
+                    for locale in locales {
+                        builder.push_record([
+                            locale.availability.to_string().as_str(),
+                            locale.code.as_str(),
+                            locale.created_at.to_string().as_str(),
+                            locale.install_date.unwrap_or("".to_string()).as_str(),
+                            locale.is_installed.to_string().as_str(),
+                            locale.is_rtl.to_string().as_str(),
+                            locale.name.as_str(),
+                            locale.native_name.as_str(),
+                            locale.updated_at.to_string().as_str(),
+                        ]);
+                    }
+                    println!("{}", builder.build().with(Style::rounded()));
+                }
+                Err(e) => {
+                    eprintln!("{}: {}", "error".bold().red(), e.to_string());
+                    std::process::exit(1);
+                }
+            }
+        },
         Command::SystemFlag { command } => match command {
             SystemFlagCommand::List {} => match api.system_flag_list() {
                 Ok(flags) => {
