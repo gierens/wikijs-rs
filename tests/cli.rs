5b8648f1a0861094d3679cb1cecd837b5478de2e
Sandro-Alessio Gierens <sandro@gierens.de>
2023-09-13 11:30:51 +0200

    test(cli): add some basic trycmd tests
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

---
+++ tests/cli.rs
@@ -0,0 +1,9 @@
+#[test]
+fn cli() {
+    trycmd::TestCases::new()
+        .case("tests/cli/*.toml");
+}
+
+// add more tests with preperation code here and put them in subfolder
+// list creating page and then showing it ...
+// some need to be made serial
