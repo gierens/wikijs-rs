c658e1b67d3770737fccdaa6ed83dc07eb39a1c4
Sandro-Alessio Gierens <sandro@gierens.de>
2023-12-04 23:22:55 +0100

    feat(cli): add password authentication as alternative to api key
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use colored::Colorize;
 use wikijs::{Api, Credentials};
 
@@ -18,6 +18,22 @@ mod user;
 
 use crate::common::Execute;
 
+#[derive(Args, Debug)]
+#[group(required = true, multiple = true)]
+struct CredentialArgs {
+    #[clap(short, long, help = "Wiki.js API key", env = "WIKI_JS_API_KEY")]
+    key: Option<String>,
+
+    #[clap(short = 'U', long, help = "Wiki.js username", env = "WIKI_JS_USERNAME", requires = "password", conflicts_with = "key")]
+    username: Option<String>,
+
+    #[clap(short = 'P', long, help = "Wiki.js password", env = "WIKI_JS_PASSWORD", requires = "username", conflicts_with = "key")]
+    password: Option<String>,
+
+    #[clap(short, long, help = "Wiki.js authentication provider ID", env = "WIKI_JS_AUTH_PROVIDER", default_value = "local")]
+    provider: Option<String>,
+}
+
 #[derive(Parser, Debug)]
 #[command(name = "wikijs-cli")]
 #[command(author = "Sandro-Alessio Gierens <sandro@gierens.de>")]
@@ -27,8 +43,8 @@ struct Cli {
     #[clap(short, long, help = "Wiki.js base URL", env = "WIKI_JS_BASE_URL")]
     url: String,
 
-    #[clap(short, long, help = "Wiki.js API key", env = "WIKI_JS_API_KEY")]
-    key: String,
+    #[clap(flatten)]
+    credentials: CredentialArgs,
 
     #[clap(subcommand)]
     command: Command,
@@ -129,7 +145,15 @@ enum Command {
 
 fn main() {
     let cli = Cli::parse();
-    let credentials = Credentials::Key(cli.key.clone());
+    let credentials = match cli.credentials.key {
+        Some(key) => Credentials::Key(key),
+        None => {
+            let username = cli.credentials.username.unwrap();
+            let password = cli.credentials.password.unwrap();
+            let provider = cli.credentials.provider.unwrap();
+            Credentials::UsernamePassword(username, password, provider)
+        }
+    };
     let api = Api::new(cli.url.clone(), credentials).unwrap_or_else(|e| {
         eprintln!("{}: {}", "error".bold().red(), e);
         std::process::exit(1);
